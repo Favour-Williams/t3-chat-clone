7619cf65ec295ea3fb4a5182f5839bb65f44c5d7
Favour-Williams <122178439+Favour-Williams@users.noreply.github.com>
2025-06-18 13:44:14 +0200

    Add files via upload

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -16,23 +16,20 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ title: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
+      return ctx.db.chat.create({
         data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
+          title: input.title,
+          userId: ctx.session.user.id,
         },
       });
     }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
+    return ctx.db.chat.findFirst({
       orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
+      where: { userId: ctx.session.user.id },
     });
   }),
 
